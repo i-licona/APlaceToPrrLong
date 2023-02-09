985aee88056eaa65bdc763c8c239ee2aaecac72f
Diego <arrabiat@outlook.com>
2023-02-09 10:42:56 -0600

    petpost controller added

---
+++ APlaceToPrrLong/DTOs/Pet/AddPetDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace APlaceToPrrLong.DTOs.Pet;
+
+public class AddPetDTO
+{
+    public DateTime AdoptionDate { get; set; }
+    public string Description { get; set; }
+    public string Nombre { get; set; }
+    public string Type { get; set; }
+    public string Status { get; set; }
+    public DateTime PostDate { get; set; }
+    public int UserId { get; set;}
+}
---
+++ APlaceToPrrLong/DTOs/Pet/PetDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace APlaceToPrrLong.DTOs.Pet;
+
+public class PetDTO
+{
+
+    private int Id { get; set; }
+
+}
